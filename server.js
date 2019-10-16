0a85454da03f4ac331a98a883f9148f9d7ab32b1
kushalkanavi <kushalkanavi@gmail.com>
2019-10-16 18:17:55 +0530

    commit

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/prod-analysis-frontend'));
 
 app.listen(process.env.PORT || 8080);
 
 //PathLocationStrategy
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/prod-analysis-frontend/index.html'))
 });
 
 console.log('Server is Running');
